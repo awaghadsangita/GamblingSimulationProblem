cf7be9ca335c9365be889ff33fe72692d8065b71
priyagund <priyankagund18@gmail.com>
2019-11-13 15:12:25 +0530

    as gambler make  bet so either win or loose

--- gamblingProgram.sh
+++ gamblingProgram.sh
@@ -4,5 +4,17 @@ read -p "Read Number of Days :"total_days
 inital_stake=100
 bet=1
 
+function  betResult()
+{
+	betValue=$((RANDOM%2))
+	if [ $betValue -eq 1 ]
+	then
+		result="WIN"
+	else
+		result="LOOSE"
+	fi
+	echo $result
+}
 
+bet_result="$( betResult )"
 
