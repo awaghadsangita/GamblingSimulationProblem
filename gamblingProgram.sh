4f9a5db0a149895a75bbc6697a66168a08eae3f9
priyagund <priyankagund18@gmail.com>
2019-11-13 15:46:18 +0530

    as gamber make bet  so either win or loose

--- gamblingProgram.sh
+++ gamblingProgram.sh
@@ -1,19 +1,28 @@
 #!/bin/bash -x
 
-read -p "Read Number of Days :"total_days
+read -p "Read Number of Days :" totalDays
 inital_stake=100
 bet=1
 
-function  betResult()
+function betResult()
 {
-	betValue=$((RANDOM%2))
-	if [ $betValue -eq 1 ]
-	then
-		result="WIN"
-	else
-		result="LOOSE"
-	fi
-	echo $result
+	for(( counter=1; counter<=$totalDays; counter++ ))
+	do
+		read -p "Enter the Number of Game" totalGame
+		for(( j=1; j<=$totalGame; j++ ))
+		do
+			betValue=$((RANDOM%2))
+			if [ $betValue -eq 1 ]
+			then
+				initial_stake=$((initial_stake+1))	
+				result="WIN"
+			else
+				initial_stake=$((initial_stake-1))
+				result="LOOSE"
+			fi
+		done
+		initial_stake=$((initial_stake+100))
+	done
 }
 
 bet_result="$( betResult )"
